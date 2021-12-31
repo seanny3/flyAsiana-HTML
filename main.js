cdce9fb053b09c56524f2d62156721d763cb3b40
seanny3 <dev.seann@gmail.com>
2021-12-31 12:00:00 +0900

    Day 03 - 1

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function fixed_notice_slider_func() {
 }
 
 // slider auto
-const auto_slider = setInterval(auto_slider_func, 4000);
+// const auto_slider = setInterval(auto_slider_func, 4000);
 
 function auto_slider_func() {
 	const slider_container = document.querySelector('.slider__img__container');
@@ -22,6 +22,7 @@ function auto_slider_func() {
 let curr_page = 0;
 const slider_left = document.querySelector('.slider__left');
 const slider_right = document.querySelector('.slider__right');
+
 slider_right.addEventListener('click', () => {
 	const slider_container = document.querySelector('.slider__img__container');
 	slider_container.style.transform = `translate(calc(-1920px*${++curr_page % 5}))`;
@@ -32,4 +33,59 @@ slider_left.addEventListener('click', () => {
 		curr_page = 5;
 	}
 	slider_container.style.transform = `translate(calc(-1920px*${--curr_page % 5}))`;
+})
+
+// contents mileage
+const mileage_btn = document.querySelector('.mileage__slider');
+mileage_btn.addEventListener('click', () => {
+	const before = document.querySelector('.mileage__slider__before');
+	before.classList.toggle('mileage__on');
+	mileage_btn.classList.toggle('mileage__on');
+})
+
+// special area slide event
+clone_slider_img();
+
+function clone_slider_img() {
+	const li = document.querySelectorAll('.specialArea__slider__img');
+	for (let i = 0; i < li.length; i++) {
+		const clone = li[i].cloneNode(true);
+		clone.classList.add('clone');
+		li[0].before(clone);
+	}
+	for (let i = 9; i >= 0; i--) {
+		const clone = li[i].cloneNode(true);
+		clone.classList.add('clone');
+		li[9].after(clone);
+	}
+}
+
+const speArea_left = document.querySelector('.specialArea__slider__left');
+const speArea_right = document.querySelector('.specialArea__slider__right');
+let speArea_curr_page = 10;
+speArea_right.addEventListener('click', () => {
+	const slider_container = document.querySelector('.specialArea__slider__img__inner');
+	if (speArea_curr_page === 20) {
+		slider_container.style.transition = "none";
+		slider_container.style.transform = `translate(calc(-312px*10))`
+		setTimeout(() => {
+			slider_container.style.transition = "all 300ms";
+			slider_container.style.transform = `translate(calc(-312px*11))`;
+		}, 30)
+		speArea_curr_page = 9;
+	}
+	slider_container.style.transform = `translate(calc(-312px*${++speArea_curr_page}))`;
+})
+speArea_left.addEventListener('click', () => {
+	const slider_container = document.querySelector('.specialArea__slider__img__inner');
+	if (speArea_curr_page === 5) {
+		slider_container.style.transition = "none";
+		slider_container.style.transform = `translate(calc(-312px*16))`
+		setTimeout(() => {
+			slider_container.style.transition = "all 300ms";
+			slider_container.style.transform = `translate(calc(-312px*15))`;
+		}, 30)
+		speArea_curr_page = 16;
+	}
+	slider_container.style.transform = `translate(calc(-312px*${--speArea_curr_page}))`;
 })
